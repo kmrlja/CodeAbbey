b663eee445633dbe9bf99f92135906bdbfa07e15
Youkai-no-nii-chan <andreychuk@mindbox.ru>
2018-05-04 21:16:07 +0300

    *

---
+++ CodeAbbey/SumLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SumAPlusB
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Прочитали первую строку, в которой хранится количество чисел.
+            var lengthInput = Console.ReadLine();
+            //проверить длину второй строки
+            
+            // Прочитали вторую строку, в которой хранятся числа через пробел.
+            var arrayInput = Console.ReadLine();
+           
+            // Разделили строку по пробелу. Получили массив строк в перемнной arrayInputValues
+            var arrayInputValues = arrayInput.Split(' ');
+            var length = int.Parse(lengthInput);
+            if (arrayInputValues.Length != length)
+            {
+                Console.WriteLine("Ошибка.");
+            }
+            else
+            {
+                var sum = 0;
+                foreach (var stringValue in arrayInputValues)
+                {
+                    var intValue = int.Parse(stringValue);
+                
+                    sum += intValue;
+                }
+            
+                //консоль выводит сумму
+                Console.WriteLine(sum);
+            }
+                
+            Console.ReadLine();
+        }
+    }
+}
+    
