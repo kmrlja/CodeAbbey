f11d9009beaa8c0c7e29ed7a79efc930e3e0bbea
Youkai-no-nii-chan <andreychuk@mindbox.ru>
2018-05-04 19:00:04 +0300

    Складываем А и Б

---
+++ CodeAbbey/SumAPlusB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumAPlusB
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var inputValues = input.Split(' ');
+            var a = int.Parse(inputValues[0]);
+            var b = int.Parse(inputValues[1]);
+            Console.WriteLine(a + b);
+
+            Console.ReadLine();
+        }
+    }
+}
